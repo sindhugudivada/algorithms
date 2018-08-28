7273844e0648b5991a3c50a422bafd6b1ca14e76
sindhu gudivada <sindhugudivada67@gmail.com>
2018-08-27 22:33:44 -0700

    Strong Password Hackerrank question.

---
+++ hacker_rank/beautiful_binary_string.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+#https://www.hackerrank.com/challenges/beautiful-binary-string/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulBinaryString function below.
+def beautifulBinaryString(b):
+    count=0
+    c = list(b)
+    for i in range(2,len(b)):
+        if c[i-2]=='0' and c[i-1] =='1' and c[i] == '0':
+            count=count+1
+            c[i]=1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(raw_input())
+
+    b = raw_input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
