cf7943e5ec3fbb9c508c50aad1a0db87ade21014
sindhu gudivada <sindhugudivada67@gmail.com>
2018-09-07 18:32:04 -0700

    array sum hackerrank question

---
+++ hacker_rank/array_sum.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+from __future__ import print_function
+
+import os
+import sys
+
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    sum = 0
+    for i in range(len(ar)):
+        sum = sum + ar[i]
+    return sum
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(raw_input())
+
+    ar = map(int, raw_input().rstrip().split())
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
